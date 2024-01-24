7ebe3ee0b4d26757ea619ccbc7f7ac3285600537
Joel47518 <100803260+Joel47518@users.noreply.github.com>
2024-01-24 03:49:54 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from PIL import Image
+import io
+import numpy as np
+
+
+def load_model():
+    model = tf.keras.models.load_model('CNN.hdf5')
+    return model
+
+
+def prep_image(filename, img_shape=224):
+    """
+  Reads an image from filename, turns it into a tensor and reshape it
+  to (img_shape, img_shape, colour_channels)
+  """
+
+    # # Read in the image
+    # img = tf.io.read_file(filename)
+    # Decode the read file into a tensor
+    img = tf.convert_to_tensor(filename, dtype=tf.float32)
+    # img = tf.image.decode_image(filename)
+    # Resize the image
+    img = tf.image.resize(img, size=[img_shape, img_shape])
+    # Rescale the image (get all values between o and 1)
+    img = img / 255.
+    return img
+
+
+def prediction(model, img, class_names):
+    """
+  Imports an image located at filename, makes a prediction with model.
+  """
+
+    # Make a prediction
+    pred = model.predict(tf.expand_dims(img, axis=0))
+
+    # Add in logic for multi-class
+    if len(pred[0]) > 1:
+        pred_class = class_names[tf.argmax(pred[0])]
+    else:
+        pred_class = class_names[int(tf.round(pred[0]))]
+
+    return pred_class
+
+
+st.title('RockID App')
+
+uploaded_file = st.sidebar.file_uploader('Upload Image')
+
+st.sidebar.divider()
+
+pred_button = st.sidebar.button("Predict", type='primary')
+
+class_names = ['Shale', 'schist', 'Conglomerate', 'Quartz_diorite', 'Quartzite', 'Limestone', 'Gneiss', 'Sandstone', 'Coal', 'Gabbro', 'Andesite', 'Amphibolite', 'Breccia', 'Basalt']
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    image = Image.open(io.BytesIO(bytes_data))
+    img_array = np.array(image)
+
+    # Display the uploaded image
+    st.image(uploaded_file, caption='Uploaded Image')
+
+    # Preprocess image
+    img_preprocessed = prep_image(img_array)
+
+    # Preprocess and make predictions
+    if pred_button:
+        predicted_class = prediction(model=load_model(), img=img_preprocessed, class_names=class_names)
+
+        # Display the prediction
+        st.markdown(f"<p style='font-size:24px'><strong>Prediction:</strong> {predicted_class}</p>", unsafe_allow_html=True)
+else:
+    st.warning("Please upload an image using the sidebar.")
